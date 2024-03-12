b303dba676c094e54e2f8446b1397e8dee25c3bb
Le Minh Bao <leminhbaost@gmail.com>
2024-03-12 11:05:43 +0700

    Quan ly ca phe

---
+++ jojo/Controllers/BaoCaoCuoiNgayController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cafe.Models;
+using System.Data.Entity;
+using Newtonsoft.Json;
+using Cafe.DTO;
+
+namespace Cafe.Controllers
+{
+    public class BaoCaoCuoiNgayController : Controller
+    {
+        //
+        // GET: /BaoCaoCuoiNgay/
+
+        CafeContext db = new CafeContext();
+        public ActionResult Index()
+        {
+            if (Request.Cookies["nhanvien"] == null)
+            {
+                return Redirect("/");
+            }
+            NhanVienDto a = JsonConvert.DeserializeObject<NhanVienDto>(HttpUtility.UrlDecode(Request.Cookies["nhanvien"].Value));//chuyen doi doi tuong json thanh C#(nhanviendto)
+            ViewBag.nhanvien = a;
+            //
+
+            DateTime ngay = DateTime.Now;
+
+            var query = from b in db.hoadons
+                        where DbFunctions.TruncateTime(b.ngayhoadon) == DbFunctions.TruncateTime(ngay)
+                        && b.active == "true"
+                        select b;
+            ViewBag.ngay = ngay.ToString("dd/MM/yyyy");
+            ViewBag.hoadon = query.ToList();
+
+            return View();
+        }
+    }
+}
---
+++ jojo/Controllers/SanPhamController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cafe.DTO;
+using Cafe.Models;
+using Newtonsoft.Json;
+
+namespace Cafe.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        //
+        // GET: /SanPham/
+        CafeContext db = new CafeContext();
+        public ActionResult Index()
+        {
+            if (Request.Cookies["nhanvien"] == null)
+            {
+                return Redirect("/");
+            }
+            NhanVienDto a = JsonConvert.DeserializeObject<NhanVienDto>(HttpUtility.UrlDecode(Request.Cookies["nhanvien"].Value));
+            if (a.loai == 1)
+            {
+                return Redirect("/KinhDoanh");
+            }
+            ViewBag.nhanvien = a;
+
+            var query = from b in db.sanphams
+                        where b.active == "true"
+                        select b;
+            ViewBag.sanpham = query.ToList();
+
+            var query2 = from b in db.sanphamphus
+                         where b.active == "true"
+                         select b;
+            ViewBag.sanphamphu = query2.ToList();
+
+            return View();
+        }
+	}
+}
